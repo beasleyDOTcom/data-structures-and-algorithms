c1b3c23b21d192b16d218499c581e8640ce03c0a
Beasleydotcom <beasley.com@gmail.com>
2020-08-12 21:04:14 -0700

    glad It sorted out

--- javascript/challenges/arrayBinarySearch/array-binary-search.js
+++ javascript/challenges/arrayBinarySearch/array-binary-search.js
@@ -1,2 +1,20 @@
 'use strict'
-//divide and conquor using the binary search method
+
+function binarySearch(arr, kilmer){
+    let firstPlace = 0;
+    let lastPlace = arr.length-1;
+    
+    while(lastPlace>=firstPlace){
+       middle = Math.floor((firstPlace + lastPlace)/2);
+      if(kilmer === arr[middle]){
+        return middle;
+      }
+      if(kilmer<arr[middle]){
+        lastPlace = middle-1;
+      }
+      else {
+        firstPlace = middle+1;
+      }
+    };
+     return -1;
+    }; 
