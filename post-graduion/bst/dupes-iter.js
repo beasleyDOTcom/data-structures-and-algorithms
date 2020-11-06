81f1dfad61a56da09d0d1729ae03ff2c9f8c6c25
Beasley <beasley.com@gmail.com>
2020-11-06 00:11:08 -0800

    added the rest of the solution code and assessment

--- post-graduion/bst/dupes-iter.js
+++ post-graduion/bst/dupes-iter.js
@@ -16,11 +16,46 @@ Output: [a, b]
 algorithm:
 define a function that takes in (BST) as an argument
 define new set named "seen"
+define an array named "resultsArray"
 define a loop that will run while the current node is truthy
-add every (truthy) value that is not null to seen
+if node.value is in seen set, add node.value to resultsArray
+else if value is (truthy) value that is not null to seen set
+node = node.next
+end of loop ****
+return resultsArray
+
+Big O:
+ - space: O(1)
+ - time: O(n)
+Testing
+ I/O
+ edge cases: not BST
 
 
 
 
 
-          */         
+
+
+
+*/         
+
+function returnDupes(BST){
+    const resultsArray = [];
+    const seen = new Set();
+    let currentNode = BST.root;
+    while(currentNode){
+        if(seen.has(currentNode.value)){
+            resultsArray.push(currentNode.value)
+        } else {
+            if(currentNode !== null){
+                seen.add(currentNode.value)
+            }
+        }
+        currentNode = currentNode.next;
+    }
+    return resultsArray;
+}
+
+
+module.exports = returnDupes;
