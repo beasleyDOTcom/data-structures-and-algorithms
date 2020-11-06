deef18f41d5dc0d9ceeb3505797ba39b437cfca3
Beasley <beasley.com@gmail.com>
2020-11-05 23:10:22 -0800

    got a good start on the algorithm

---
+++ post-graduion/bst/dupes-iter.js
@@ -0,0 +1,26 @@
+/*
+--problem domain:
+given a binary search tree return a list of all of the duplicate keys/values -- iteratively.
+
+--I/O
+input       a
+          /   \
+        a       c
+          \    /  \   
+           b  b     d
+
+
+Output: [a, b]
+
+
+algorithm:
+define a function that takes in (BST) as an argument
+define new set named "seen"
+define a loop that will run while the current node is truthy
+add every (truthy) value that is not null to seen
+
+
+
+
+
+          */         
