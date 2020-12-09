c13aee9bc9dbe12118c6e2479997d11246a4510f
Beasley <beasley.com@gmail.com>
2020-12-08 21:33:12 -0800

    working on an old challenge 'multi bracket validation'

--- javascript/challenges/multiBracketValidation/__tests__/multi-bracket-validation.test.js
+++ javascript/challenges/multiBracketValidation/__tests__/multi-bracket-validation.test.js
@@ -1,10 +1,14 @@
 'use strict'
 const testString = '[test}'
-const bracketTown = require('../multi-bracket-validation.js');
+const validateBracketTown = require('../multi-bracket-validation.js');
 it('should return false if ', () => {
-    expect(bracketTown(testString)).toBe(false);
+    expect(validateBracketTown(testString)).toBe(false);
 });
 
-it('should ', () => {
-    expect(bracketTown(['[{[]}]'])).toBeTruthy()
+it.only('should be true with a bunch of balanced brackets', () => {
+    expect(validateBracketTown(['{}()[{[]}]'])).toBe(true);
+});
+it('should return true with ()', () => {
+    let string = '()';
+    expect(validateBracketTown(string)).toBe(true);
 });
