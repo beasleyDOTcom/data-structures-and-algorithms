1f0cd987e2692dfcc9e62d3d8e9437644623f94d
Beasley <beasley.com@gmail.com>
2020-12-08 19:20:53 -0800

    added fibonacci whiteboard and wrote tests to prove its' functionality

---
+++ post-graduion/whiteboards/fibonacci.test.js
@@ -0,0 +1,14 @@
+const addUp = require('./fibonacci.js');
+
+it('should return a number', () => {
+    expect(typeof(addUp(3))).toBe('number');
+});
+it('should return 3 if called with 5', () => {
+    expect(addUp(5)).toBe(3);
+});
+it('should return 1 if called with 2', () => {
+    expect(addUp(2)).toBe(1);
+});
+it('should return "not a fibonacci number! Please enter an integer equal to or higher than 1" if called with a non fibonacci number', () => {
+    expect(addUp(0)).toBe('not a fibonacci number! Please enter an integer equal to or higher than 1');
+});
