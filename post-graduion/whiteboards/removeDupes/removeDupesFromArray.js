b95c38ab840efb80384ccb6e6ef8f6bfc47d3636
beasleyDotcom <beasley.com@gmail.com>
2020-12-15 13:39:38 -0800

    added practice whiteboard interview with Tia

---
+++ post-graduion/whiteboards/removeDupes/removeDupesFromArray.js
@@ -0,0 +1,41 @@
+/*
+Whiteboard with Tia
+Problem domain:
+given an array, remove all of the duplicate values -- iteratively.
+
+input / output:
+array of numbers
+mutate original array to return array without dupes.
+input: [1,3,4,1,2,3,4,2]
+output: [1,3,4,2]
+[1,3,4,2,3,4]
+Algorithm:
+look at every element of array.
+if element is in seen set then splice that element out of array. 
+else add element to seen set and move on to next element.
+seen set = [1,3,4,2]
+i = 4;
+code:
+*/
+function removeDupes(arr){
+	let seen = new Set();
+	for(let i = 0; i < arr.length; i++){//7
+		if(seen.has(arr[i])){
+	while(seen.has(arr[i])){
+arr.splice(i,1);
+}
+if(!seen.has(arr[i])){ seen.add(arr[i])}
+} else {
+	seen.add(arr[i]);
+}
+}
+return arr;
+} 
+/*
+Testing: so try to walk through this equation and mess it up so that I an bolster its' ability to handle various situations.
+
+Big O:
+time: O(n)
+space: O(n)
+obj[1] === undefined ? return false : return true;
+*/
