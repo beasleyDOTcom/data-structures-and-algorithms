e4c07dff8e386a1745b0fe4c6ce74c5efdab61d6
Beasley <beasley.com@gmail.com>
2020-12-17 17:25:01 -0800

    wrote solution code to pass all tests

--- post-graduion/stack/getMaxIter/__tests__/getMaxIter.test.js
+++ post-graduion/stack/getMaxIter/__tests__/getMaxIter.test.js
@@ -4,7 +4,7 @@ describe('the getMax() method should return the number with the highest value fr
     it('should return a number', () => {
         const numberStack = new Stack();
         numberStack.push(1);
-        expect(typeof(numberStack.getMax())).toBe(number)
+        expect(typeof(numberStack.getMax())).toBe('number')
     });
     it('should return number if stack only has one number.', () => {
         const numberStack = new Stack();
@@ -25,5 +25,19 @@ describe('the getMax() method should return the number with the highest value fr
     it('should return null if stack is empty', () => {
         const numberStack = new Stack();
         expect(numberStack.getMax()).toBeNull();
+    });
+    it('should allow pop() and peek() to work as expected', () => {
+        const numberStack = new Stack();
+        numberStack.push(1);
+        numberStack.push(11);
+        numberStack.push(111);
+        numberStack.push(122);
+        numberStack.push(12);
+        numberStack.push(144);
+        numberStack.push(1455);
+        expect(numberStack.pop()).toBe(1455);
+        expect(numberStack.pop()).toBe(144);
+        expect(numberStack.pop()).toBe(12);
+        expect(numberStack.peek()).toBe(122);
     });
 });
---
+++ post-graduion/stack/getMaxIter/getMaxIter.js
@@ -0,0 +1,49 @@
+'use strict';
+// citizen 4
+// snowden
+class Stack {
+    constructor(){
+        this.top = null;
+    }
+    push(item){
+        this.top = new Node(item, this.top)
+    }
+    pop(){
+        if(this.top === null){return 'stack is empty';}
+        let temp = this.top.value;
+        this.top = this.top.next;
+        return temp;
+    }
+    isEmpty(){
+        if(this.top === null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    peek(){
+        if(!this.isEmpty()){
+            return this.top.value;
+        }
+    }
+    getMax(){
+        if(this.isEmpty()){return null;}
+        let top = this.top;
+        let max = this.top.value;
+        while(top !== null){
+            if(max < top.value){
+                max = top.value; 
+            }
+            top = top.next; 
+        }
+        return max;
+    }
+}
+class Node{
+    constructor(value, next=null){
+        this.value = value;
+        this.next = next;
+    }
+}
+
+module.exports = Stack;
