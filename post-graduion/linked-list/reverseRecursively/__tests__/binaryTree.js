2d47c54dc9e65b956ccb8160e72f63ace31bdd82
Beasley <beasley.com@gmail.com>
2020-12-28 22:27:20 -0800

    got started on creating a binary tree function to support functioning

---
+++ post-graduion/linked-list/reverseRecursively/__tests__/binaryTree.js
@@ -0,0 +1,21 @@
+'use strict';
+class Node{
+    constructor(value, left=null, right=null){
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+class BinaryTree{
+    constructor(){
+        this.root = null;
+    }
+    add(value){
+        if(this.root === null){
+            this.root= new Node(value);
+        } else {
+            let currentNode = this.root;
+            while(currentNode.)
+        }
+    }
+}
