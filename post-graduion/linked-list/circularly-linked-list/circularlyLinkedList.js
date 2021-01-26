be28ba1d884d102ac906f32ac2e939c8a7ea769c
Beasley <beasley.com@gmail.com>
2021-01-26 13:32:42 -0800

    solution code to pass all tests for addPlayer()

--- post-graduion/linked-list/circularly-linked-list/circularlyLinkedList.js
+++ post-graduion/linked-list/circularly-linked-list/circularlyLinkedList.js
@@ -11,12 +11,49 @@ class LinkedList {
     }
     addPlayer(person){
         if(!this.head){
-            this.head = new Node(person, this.head);
+            this.head = new Node(person, this.tail);
             this.tail = this.head;
+            this.tail.next = this.head;
         } else {
             this.head = new Node(person, this.head);
             this.tail.next = this.head;
         }
+    }
+    removePlayer(player){
+        if(!this.head){return null};          // null check
+
+        if(this.head.value === player){       // first node is target
+            
+            if(this.head === this.tail){ // list has one item that points to itself.
+                this.head = null;
+                this.tail = null;
+                return `Removed ${player} from List`;
+            } else {
+                this.head = this.head.next;                          // list has more than one item
+                this.tail.next = this.head.next; // adjust circular reference to second item in list
+                return `Removed ${player} from List`;
+            }
+        }
+// at this point we know that there is a head and it is not the target.
+        let a = this.head;
+        let b = this.head.next;
+       
+        while(this.tail !== a){
+            if(b.value === player){
+                return `Removed ${player} from List`;
+            } else {
+                b = b.next;
+                a = a.next;
+            }
+        }
+        return 'Player Not Found';
+        // chad --> jenny --> steven --> chad -->
+        // chad --> steven --> chad --> 
+        // is player in list?
+        // if so, remove player
+        // - you need to work with a, b, c's
+        // a.next = c;
+        // if not, return 'player not found!'
     }
     
 }
