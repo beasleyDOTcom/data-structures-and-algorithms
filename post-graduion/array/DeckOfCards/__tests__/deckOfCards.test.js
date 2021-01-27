0b93c44873c64e30d729cecf4907854cc4832605
Beasley <beasley.com@gmail.com>
2021-01-27 14:19:59 -0800

    further developed test suite to prescribe solution code

--- post-graduion/array/DeckOfCards/__tests__/deckOfCards.test.js
+++ post-graduion/array/DeckOfCards/__tests__/deckOfCards.test.js
@@ -1,19 +1,23 @@
+
+
 const Deck = require('../deckOfCards.js');
 
-const cards = new Deck();
+const deck = new Deck();
 
-describe('prove the ability to simulate a deck of cards', () => {
+describe('.add()', () => {
     it('should add cards to the "deck"', () => {
-    cards.add('tools');
-    cards.add('sheers');
-    cards.add('people');
-    cards.add('myspace');
-    cards.add('soundCloud');
-    cards.add('paypal');
-    expect(cards[0])        
+    deck.add('tools');
+    deck.add('sheers');
+    deck.add('people');
+    deck.add('myspace');
+    deck.add('soundCloud');
+    deck.add('paypal');
+    expect(deck.cards[0]).toStrictEqual('tools');
+    expect(deck.cards[5]).toStrictEqual('paypal');       
     });
 });
-describe('shuffle', () => {
+
+describe('.shuffle() method', () => {
 
     function same(a, b){
         for(let i = 0; i < a.length; i++){
@@ -23,8 +27,28 @@ describe('shuffle', () => {
         }
         return true;
     }
-
+    const arr = [1, 4, 3, 2];
+    
     it('should not return same array', () => {
-        
+        expect(same([...deck.cards], deck.shuffle())).toBeFalse();
+    });
+    it('should ', () => {
+        expect(...deck.cards).not.toStrictEqual(deck.shuffle())
+    });
+});
+
+describe('.remove() method', () => {
+    const testDeck = new Deck();
+    deck.add('check into cash');
+    deck.add('olympics');
+    deck.add('emergen-c')
+    deck.add('pay pal')
+    it('should reduce length by 1', () => {
+        expect(deck.remove(2)).toStrictEqual('emergen-c');
+        expect(deck.cards).toHaveLength(3);
+    });
+    it('should remove an item and preserve relative of other elemenths', () => {
+        expect(deck.cards[2]).toStrictEqual('pay pal');
+        expect(deck.cards[1]).toStrictEqual('olympics');
     });
 });
