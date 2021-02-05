0fbc2a61b17e0e870adfbce35e0caf79e4ca74be
Beasley <beasley.com@gmail.com>
2021-02-05 13:30:19 -0800

    added solution code and updated tests to achieve 100% coverage

--- javascript/challenges/post-graduion/BigNumbers/__tests__/addBigNumbers.test.js
+++ javascript/challenges/post-graduion/BigNumbers/__tests__/addBigNumbers.test.js
@@ -7,7 +7,7 @@ describe('Does addBig successfully take in two strings and return their sum as a
         expect(addBig('1', '1')).toStrictEqual('2');
     });
     it('should add larger number', () => {
-        expect(addBig('10000001', '99999999')).toStrictEqual('20000000');
+        expect(addBig('10000001', '99999999')).toStrictEqual('110000000');
     });
     it('should add a short string and long string', () => {
         expect(addBig('1', '9999')).toStrictEqual('10000');
@@ -16,6 +16,12 @@ describe('Does addBig successfully take in two strings and return their sum as a
         expect(addBig('999999','1')).toStrictEqual('1000000');
     });
     it('should add BIG NUMBERS!', () => {
-        expect(addBig('9999999999999999999999999999999999999999999999','1000000000000000000000000000000000000000000001')).toStrictEqual('10000000000000000000000000000000000000000000000')
+        expect(addBig('9999999999999999999999999999999999999999999999','1000000000000000000000000000000000000000000001')).toStrictEqual('11000000000000000000000000000000000000000000000');
+    });
+    it('should add lopsided numbers that do not roll over', () => {
+        expect(addBig('10000000000000000000000000000000000000000000000000','1')).toStrictEqual('10000000000000000000000000000000000000000000000001');
+    });
+    it('should add alternately lopsided numbers that do not roll over', () => {
+        expect(addBig('1','10000000000000000000000000000000000000000000000000')).toStrictEqual('10000000000000000000000000000000000000000000000001');
     });
 });
