540cb04f7c4960514d59fe6276744ceb237993dc
Beasley <beasley.com@gmail.com>
2021-02-16 18:38:47 -0800

    not done but needed to go

---
+++ javascript/challenges/codeWars/rangeExtraction/rangeExtraction.js
@@ -0,0 +1,7 @@
+ // look at every item in list
+  // if item +1 === next item && item +2 equals next.next
+  // if false - push number to resultArr
+  // if true - a range is in order
+      // we want to start range with original.toString()
+      // + ''-'+lastIncremented.toString()
+  // 
