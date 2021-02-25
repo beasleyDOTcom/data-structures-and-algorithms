89ea6880d6c2de51df3bee436b5c3835e25ee28a
Beasley <beasley.com@gmail.com>
2021-02-24 23:58:41 -0800

    got a solution

--- javascript/challenges/codeWars/pinProjection/__tests__/pinProjection.test.js
+++ javascript/challenges/codeWars/pinProjection/__tests__/pinProjection.test.js
@@ -1,5 +1,8 @@
 const pin = require('../pinProjection.js');
 
-it('should ', () => {
-    expect(true).toBe(true);
+it('should handle one character', () => {
+    expect(pin('8')).toStrictEqual(expect.arrayContaining(['0','9','7','5','8']))
+});
+it('should handle two characters', () => {
+    expect(pin('00')).toStrictEqual(expect.arrayContaining(['08','80']))
 });
--- javascript/challenges/codeWars/pinProjection/pinProjection.js
+++ javascript/challenges/codeWars/pinProjection/pinProjection.js
@@ -1,18 +1,68 @@
-'use strict'
+'use strict';
 
 function getPINs(observed) {
-    const result = [];
-    function replaceNumber(index, replacement, ...string){
-      result.push(string.splice(index,1, replacement))
-    }
-    for(let i = 0; i < observed.length; i++){
-      switch(observed[i]){
-          case '1':
-          replaceNumber()
-          break;
-      }
-    }
-    return [11,2]
-  }
 
-  module.exports = getPINs;
+    const result = [];
+    function replaceNumber(index, replacement, string) {
+        if(index > 0){
+            result.push(string.slice(0,index) + replacement + string.slice(index +1))
+        } else {
+            result.push(replacement + string.slice(1))            
+        }
+    }
+    result.push(observed)
+
+    for (let i = 0; i < observed.length; i++) {
+        switch (observed[i]) {
+            case '1':
+                replaceNumber(i, '2', observed);
+                replaceNumber(i, '4', observed);
+                break;
+            case '2':
+                replaceNumber(i, '1', observed);
+                replaceNumber(i, '5', observed);
+                replaceNumber(i, '3', observed);
+                break;
+            case '3':
+                replaceNumber(i, '2', observed);
+                replaceNumber(i, '6', observed);
+                break;
+            case '4':
+                replaceNumber(i, '1', observed);
+                replaceNumber(i, '5', observed);
+                replaceNumber(i, '7', observed);
+                break;
+            case '5':
+                replaceNumber(i, '2', observed);
+                replaceNumber(i, '4', observed);
+                replaceNumber(i, '6', observed);
+                replaceNumber(i, '8', observed);
+                break;
+            case '6':
+                replaceNumber(i, '3', observed);
+                replaceNumber(i, '5', observed);
+                replaceNumber(i, '9', observed);
+                break;
+            case '7':
+                replaceNumber(i, '4', observed);
+                replaceNumber(i, '8', observed);
+                break;
+            case '8':
+                replaceNumber(i, '5', observed);
+                replaceNumber(i, '7', observed);
+                replaceNumber(i, '9', observed);
+                replaceNumber(i, '0', observed);
+                break;
+            case '9':
+                replaceNumber(i, '8', observed);
+                replaceNumber(i, '6', observed);
+                break;
+            default:
+                replaceNumber(i, '8', observed);
+
+        }
+    }
+    return result;
+}
+
+module.exports = getPINs;
