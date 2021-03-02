cfca922b493dc4c9e8b4c28a390de509523e8caf
Beasley <beasley.com@gmail.com>
2021-03-02 12:52:17 -0800

    started daily leetcode challenge

---
+++ javascript/challenges/leetCode/eatCandies.js
@@ -0,0 +1,11 @@
+'use strict';
+// given an array of ints return max number of unique numbers that may be eaten if length / 2
+// how many types of candies are there?
+// how many are allowed to be eaten?
+// if candy types < allowed to be eaten, return candy types
+// else if return allowed to be eaten.
+var distributeCandies = function(candyType) {
+    let types = {};
+    
+        
+};
