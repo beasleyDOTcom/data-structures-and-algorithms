ff09bcc7ab3d013d0c69e76c453456c32450549e
Beasley <beasley.com@gmail.com>
2021-03-14 20:49:08 -0700

    fastern than 99% of submissions on leetcode

--- javascript/challenges/leetCode/greatestNumberOfCandies/__tests__/greatestNumerOfCandies.test.js
+++ javascript/challenges/leetCode/greatestNumberOfCandies/__tests__/greatestNumerOfCandies.test.js
@@ -7,12 +7,12 @@ describe('should return list of boolean values representing whether or not each 
         let candies = [2,3,5,1,3];
         let extraCandies = 3;
         let output = [true,true,true,false,true];
-        expect(candyBarron(candies, extraCandies)).toBe(output); 
+        expect(candyBarron(candies, extraCandies)).toStrictEqual(output); 
     });
     it('should work on second example', () => {
         let candies = [4,2,1,1,2];
         let extraCandies = 1;
         let output = [true,false,false,false,false];
-        expect(candyBarron(candies, extraCandies)).toBe(output); 
+        expect(candyBarron(candies, extraCandies)).toStrictEqual(output); 
     });
 });
