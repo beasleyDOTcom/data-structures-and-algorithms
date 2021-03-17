ee0222e7c3c0235e55b6fba8dc9a881307bc808c
Beasley <beasley.com@gmail.com>
2021-03-17 11:22:55 -0700

    80% less memory and faster than 95% of javaScript submissions.

--- javascript/challenges/leetCode/maximumWealth/__tests__/maximumWealth.test.js
+++ javascript/challenges/leetCode/maximumWealth/__tests__/maximumWealth.test.js
@@ -9,4 +9,8 @@ describe('return the wealth of the richest customer', () => {
         let accounts = [[1,5],[7,3],[3,5]];
         expect(maxWealth(accounts)).toStrictEqual(10);
     });
+    it('should pass third basic test', () => {
+        let accounts = [[2,8,7],[7,1,3],[1,9,5]];
+        expect(maxWealth(accounts)).toStrictEqual(17);
+    });
 });
