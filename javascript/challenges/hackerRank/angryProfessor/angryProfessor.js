dc80fde671d0fe53bb0f0df19889e74c3df4813f
Beasley <beasley.com@gmail.com>
2021-04-26 12:47:29 -0700

    added solution to angry professor code challenge

--- javascript/challenges/hackerRank/angryProfessor/angryProfessor.js
+++ javascript/challenges/hackerRank/angryProfessor/angryProfessor.js
@@ -1,8 +1,25 @@
 'use strict';
 
 function angryProfessor(k, a) {
-    // Write your code here
+    // basic algorithm:
 
+    /*
+    studentsOnTime = 0;
+    for loop through array (a)
+    if studentsOnTime >= k return 'YES'
+    if a[i] <= 0 studentsOnTime++
+
+    */
+    let studentsOnTime = 0;
+    for(let i = 0; i < a.length; i++){
+        if(a[i] <= 0){
+            studentsOnTime++;
+        }
+        if(studentsOnTime >= k){
+            return 'YES';
+        }
+    }
+    return 'NO';
 }
 
 
