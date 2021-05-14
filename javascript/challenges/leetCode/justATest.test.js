e3eb38e2bd2b5814ae7122441d8f2d71f1ef359f
Beasley <beasley.com@gmail.com>
2021-05-13 21:11:43 -0700

    finished whiteboarding and got to use my new alias jstest

--- javascript/challenges/leetCode/justATest.test.js
+++
@@ -1,8 +0,0 @@
-const justATest = require('../justATest.js');
-
-describe('Basic test of justATest', () => {
-    it('should import justATest', () => {
-        expect(justATest()).not.toBeNull();
-    });
-});
-
