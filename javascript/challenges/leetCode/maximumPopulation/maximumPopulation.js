42804ceb2969ad07defd793dc1d94e46322206ef
Beasley <beasley.com@gmail.com>
2021-05-14 13:41:59 -0700

    getting started on a fun little challenge

---
+++ javascript/challenges/leetCode/maximumPopulation/maximumPopulation.js
@@ -0,0 +1,16 @@
+'use strict'
+// how to do this?
+// how do we find the years that the most people were alive?
+// in other words, how many sub arrays share some of the same range?
+// for each sub array, how many sub arrays does it share range with?
+// of those, which is the earliest date of their appearance?
+// I wonder if there is a way to sort of lay these down on top of each other and then find the earliest occurance of the highest stack.
+// 1995 1996 1997 1998 1999
+   //   1996 1997
+// 1995 1996 1997 1998
+// 1996
+ function maximumPopulation(logs){ 
+    if(logs.length ===1){return logs[0][0]} 
+    return
+ } 
+ module.exports = maximumPopulation
