958731f22426dc9956646294d21d342f67f4ad5c
Beasley <beasley.com@gmail.com>
2021-05-18 23:48:51 -0700

    bolstered testing suite to include edge cases. Achieved 100% coverage.

--- javascript/challenges/leetCode/maximumPopulation/maximumPopulation.js
+++ javascript/challenges/leetCode/maximumPopulation/maximumPopulation.js
@@ -10,14 +10,16 @@
 // 1995 1996 1997 1998
 // 1996
 //[[1995,1999],[1996,1998],[1995,1998],[1996,2000]]
+// algorithm:
+// narray down the years to search by 
+// found 
 
-
-// [ [1950,1961],   [1960,1971],   [1970,1981]]
+// ---------->>> [ [1950,1961],   [1960,1971],   [1970,1981]]
  function maximumPopulation(logs){ 
    if(logs.length ===1){return logs[0][0]} 
    // find earliest birth year in population
    let currentYear = logs[0][0];  // ----->    1950
-   // find highest death year in population -> 1961
+   // find highest death year in population -> 1961 -> 1971, -> 1981
    let highest = logs[0][1];
    for(let i = 1; i < logs.length; i++){
       // find earliest year in population
