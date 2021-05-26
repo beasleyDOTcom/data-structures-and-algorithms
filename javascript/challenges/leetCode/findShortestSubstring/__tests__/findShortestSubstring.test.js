8aaca4fa3d4ed228bed9169cad88fd91dfcd4d29
Beasley <beasley.com@gmail.com>
2021-05-25 21:18:20 -0700

    just got setup starteed

---
+++ javascript/challenges/leetCode/findShortestSubstring/__tests__/findShortestSubstring.test.js
@@ -0,0 +1,8 @@
+const findShortestSubstring = require('../findShortestSubstring.js');
+
+describe('Basic test of findShortestSubstring', () => {
+    it('should import findShortestSubstring', () => {
+        expect(findShortestSubstring()).not.toBeNull();
+    });
+});
+
---
+++ javascript/challenges/leetCode/findShortestSubstring/findShortestSubstring.js
@@ -0,0 +1,5 @@
+'use strict'
+ function findShortestSubstring(){ 
+     return
+ } 
+ module.exports = findShortestSubstring
