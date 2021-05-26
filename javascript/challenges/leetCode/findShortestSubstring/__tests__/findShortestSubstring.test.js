2f4fb5818c373afc10d2cda9d433daea728d2e2e
Beasley <beasley.com@gmail.com>
2021-05-25 22:04:50 -0700

    added a couple test

--- javascript/challenges/leetCode/findShortestSubstring/__tests__/findShortestSubstring.test.js
+++ javascript/challenges/leetCode/findShortestSubstring/__tests__/findShortestSubstring.test.js
@@ -2,7 +2,23 @@ const findShortestSubstring = require('../findShortestSubstring.js');
 
 describe('Basic test of findShortestSubstring', () => {
     it('should import findShortestSubstring', () => {
+
         expect(findShortestSubstring()).not.toBeNull();
+    });
+    
+    it('should just join things if there\'s nothing to do', () => {
+        
+// Input: words = ["alex","loves","leetcode"]
+// Output: "alexlovesleetcode"
+        let words = ["alex","loves","leetcode"];
+        let output = "alexlovesleetcode";
+        expect(findShortestSubstring(words)).toStrictEqual(output);
+    });
+    it('should reduce string as much as possible', () => {
+     let words = ["catg","ctaagt","gcta","ttca","atgcatc"];
+     let output = "gctaagttcatgcatc";
+
+        expect(findShortestSubstring(words)).toStrictEqual(output);
     });
 });
 
