18a4bb01284da1e44032ec37e0787d0d38bfdcfd
Beasley <beasley.com@gmail.com>
2021-06-23 19:58:22 -0700

    got started on testing today

--- python/whiteboards/kth_largest_in_bst/tests/test_kth_largest_in_bst.py
+++ python/whiteboards/kth_largest_in_bst/tests/test_kth_largest_in_bst.py
@@ -8,28 +8,46 @@ class Node:
 
   
 class BST:
-  def __init__(self, root=None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def add(self, val):
-    if self.root == None:
-      self.root = Node(val)
-    else:
-      current = self.root
-      while current != None:
-        if val == current.value:
-          temp = current.right
-          current.right = Node(val,None, temp)
-        elif val < current.value:
-          current = current.left
+    def add(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+            keep_looking = True
+            while keep_looking:
+                if val == current.value:
+                    temp = current.right
+                    current.right = Node(val,None, temp)
+                    keep_looking = False
+                elif val < current.value:
+
+                    if current.left == None:
+                        current.left = Node(val)
+                    else:
+                        current = current.left
+                else:
+                    # val > current.value
+                    if current.right == None:
+                        current.right = Node(val)
+                    else:
+                        current = current.right
+                    
 
 
 
 
-def test_import_of_name():
-    expected = 'True'
-    actual = kth_largest_in_bst(True)
-    assert actual == expected
+def test_add():
+    bst = BST(Node(15))
+    
+    bst.add(5)
+    bst.add(20)
+
+    assert bst.root.value == 15
+    assert bst.root.left.value == 5
+    assert bst.root.right.value == 20
     
 def test_none_tree():
     bst = BST(Node())
