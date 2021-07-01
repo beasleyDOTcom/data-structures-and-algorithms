4ea675754d63159c45058d29bb7f549bb3ce9ad5
Beasley <beasley.com@gmail.com>
2021-07-01 12:36:50 -0700

    added the built in method

--- python/whiteboards/append_in_place/tests/append_in_place.py
+++ python/whiteboards/append_in_place/tests/append_in_place.py
@@ -1,3 +1,5 @@
+from . stack import Stack
+
 """
 
 Write a function that accepts two arrays. Append the values from the second array into the first,
@@ -6,10 +8,15 @@ maintaining the ordering.
 The function should modify the first array in place. Do not use a return statement.
 
 For example:
-const a = [1, 2]; NOTE: If you assign an array to a `const`, you can't re-assign it later, but you can change the values in the array.
-const b = [3, 4];
-append(a, b);
-console.log(a) prints [1, 2, 3, 4]
+a = [1, 2]; NOTE: If you assign an array to a `const`, you can't re-assign it later, but you can change the values in the array.
+b = [3, 4];
+append_in_place(a, b);
+print(a) prints [1, 2, 3, 4]
+
+Questions:
+Can I presume both a and b will be Arrays?  --> Yes. Both arrays will be one dimensional arrays.
+Should the second input array (b) be mutated? --> Yes. All items should be removed print(b) prints: []
+
 
 # algorithm:
 remove items from second array:
@@ -22,46 +29,11 @@ pop every item in stack and append to array
 """
 
 def append_in_place(a,b):
-
-    # create node class
-    class Node:
-        def __init__(self, value=None, next=None):
-            self.value = value
-            self.next = next            
-
-    # create stack
-    class Stack:
-        def __init__(self):
-            self.top = None
-        
-        def isEmpty(self):
-            if self.top == None:
-                return True
-            else:
-                return False
-
-        def peek(self):
-            if self.isEmpty():
-                raise Exception('cannot peek empty stack')
-            else:
-                return self.top.value
-
-        def push(self, val):
-            self.top = Node(val, self.top)
-            return True
-        
-        def pop(self):
-            if self.isEmpty():
-                raise Exception('cannot pop empty stack')
-            else:
-                temp = self.top.value
-                self.top = self.top.next
-                return temp
-
+    # a.extend(b)
     stack = Stack()
     # push items from second array into stack
-    for i in reversed(range(len(b))):
-        stack.push(b[i])
+    for i in range(len(b)):
+        stack.push(b.pop())
 
     while(not stack.isEmpty()):
         # pop every item in stack and append to first array
