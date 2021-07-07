7b24df2ed7236c0244aecccd48778557ba70831f
Beasley <beasley.com@gmail.com>
2021-07-07 16:07:45 -0700

    still having trouble figuring out the obj.update method

--- python/whiteboards/airport_code/tests/test_airport_code.py
+++ python/whiteboards/airport_code/tests/test_airport_code.py
@@ -1,6 +1,18 @@
 from . airport_code import airport_code
-def test_import_of_name():
-    expected = 'True'
-    actual = airport_code(True)
-    assert actual == expected
+
+def test_one_code():
+    input = ['dfw']
+    actual = airport_code(input)
+    print(actual.hash_map_of_airport_codes)
+    assert actual.hash_map_of_airport_codes['d']['f']['w'] == 'dfw'
+
+
+    
+def test_multiple_codes():
+    input = ['atl', 'ats', 'atw', 'aty']
+    actual = airport_code(input)
+    airports_list = actual.hash_map_of_airport_codes
+    print(airports_list)
+    print('this is actual: ', actual.hash_map_of_airport_codes)
+    assert airports_list.includes('atw')
     
