b88b0c25eebc0f68957a8ccd646961df33e7b49b
Beasley <beasley.com@gmail.com>
2021-07-29 13:06:01 -0700

    added solution code and a few more tests

--- python/leetCode/add_two_numbers_from_lists/tests/add_two_numbers_from_lists.py
+++ python/leetCode/add_two_numbers_from_lists/tests/add_two_numbers_from_lists.py
@@ -12,62 +12,65 @@ algorithm:
     represent sum as ll in reverse
 
 """
-class Node():
+class ListNode():
     def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
 
 
-class LinkedList():
-    def __init__(self):
-        self.root=None
+# class LinkedList():
+#     def __init__(self):
+#         self.root=None
     
-    def add(self, val):
-        temp = self.root
-        if temp == None:
-            temp = Node(val)
-        elif temp.next == None:
-            temp.next = Node(val)
-        while temp.next != None:
-            temp = temp.next
+#     def add(self, val):
+#         temp = self.root
+#         if temp == None:
+#             temp = ListNode(val)
+#         elif temp.next == None:
+#             temp.next = ListNode(val)
+#         while temp.next != None:
+#             temp = temp.next
         
-        # we have reached the last valid node in list. add new node after that
-        temp.next = Node(val)        
-
-def build_list(string_num):
-    
-    list = LinkedList()
-
-    i = len(string_num)-1
-    while i >= 0:
-        list.add(int(string_num[i]))
-        i -= 1
-    
-    return list
+#         # we have reached the last valid node in list. add new node after that
+#         temp.next = ListNode(val)        
 
 
 def add_two_numbers_from_lists(node1, node2):
-    print('this is node1: ', node1)
-    print('this is node2: ', node2)
-
-
-    def add_one_list(node1):
+    def build_list(string_num):
+        """
+        Ok, so we want to build a linked list out of this string_num in reverse so that the last char in string_num is the value of the first node of ListNodes.
+        make a node for 
+        """
+        i = len(string_num)-1
+        node = ListNode(int(string_num[i]))
+        head = node
+        i -= 1
+        while i >= 0:
+            node.next = ListNode(int(string_num[i]))
+            i -= 1
+            node = node.next
+        return head
+    def add_one_list(node):
         # define a multiplier that starts out as 1.
-        print(node1)
+       
         multiplier = 1
         sum = 0
-        if node1.next == None:
+        if node.next == None:
             # there is only one number represented in list --> add it to sum and move on. else you have work to do.
-            sum += node1.val
+            sum += node.val
         else:
-            while node1.next != None:
-                sum += node1.val * multiplier
+            while node.next != None:
+                print('this is node: ', node.val)
+                sum += int(node.val) * multiplier
+                print('here', sum)
                 multiplier *= 10
-            # you still need to add node1.next
-            sum += node1.next.val * multiplier
+                node = node.next
+            # you still need to add node.next
+            sum += node.val * multiplier
+        return sum
+
     sum = add_one_list(node1) + add_one_list(node2)
 
     # represent sum as ll in reverse
     result = build_list(str(sum))
-    return result.root
-    
+    return result
--- python/leetCode/add_two_numbers_from_lists/tests/test_add_two_numbers_from_lists.py
+++ python/leetCode/add_two_numbers_from_lists/tests/test_add_two_numbers_from_lists.py
@@ -1,41 +1,50 @@
 from . add_two_numbers_from_lists import add_two_numbers_from_lists
 
 
-class Node():
+# class Node():
+#     def __init__(self, val=None, next=None):
+#         self.val = val
+#         self.next = next
+
+
+# class LinkedList():
+#     def __init__(self):
+#         self.root=None
+    
+#     def add(self, val):
+#         temp = self.root
+#         if temp == None:
+#             self.root = Node(val)
+#             return
+#         elif temp.next == None:
+#             temp.next = Node(val)
+#             return
+#         while temp.next != None:
+#             temp = temp.next
+        
+#         # we have reached the last valid node in list. add new node after that
+#         temp.next = Node(val)        
+
+class ListNode():
     def __init__(self, val=None, next=None):
         self.val = val
         self.next = next
 
-
-class LinkedList():
-    def __init__(self):
-        self.root=None
-    
-    def add(self, val):
-        temp = self.root
-        if temp == None:
-            self.root = Node(val)
-            return
-        elif temp.next == None:
-            temp.next = Node(val)
-            return
-        while temp.next != None:
-            temp = temp.next
-        
-        # we have reached the last valid node in list. add new node after that
-        temp.next = Node(val)        
-
 def build_list(string_num):
-    
-    list = LinkedList()
-
+    """
+    Ok, so we want to build a linked list out of this string_num in reverse so that the last char in string_num is the value of the first node of ListNodes.
+    make a node for 
+    """
     i = len(string_num)-1
+    node = ListNode(int(string_num[i]))
+    head = node
+    i -= 1
     while i >= 0:
-        list.add(int(string_num[i]))
+        node.next = ListNode(int(string_num[i]))
         i -= 1
-    
-    return list
-
+        node = node.next
+    print('this is head from build list: ', head.val)
+    return head
 
 """
 Input: l1 = [2,4,3], l2 = [5,6,4]
@@ -65,10 +74,10 @@ def test_first_linked_list():
 
     list2 = build_list('465')
 
-    node = add_two_numbers_from_lists(list1.root, list2.root)
-    assert node.val == '7'
-    assert node.next.val == '0'
-    assert node.next.next.val == '8' 
+    node = add_two_numbers_from_lists(list1, list2)
+    assert node.val == 7
+    assert node.next.val == 0
+    assert node.next.next.val == 8 
 
 
 def test_second_case():
@@ -76,8 +85,8 @@ def test_second_case():
 
     list2 = build_list('0')
 
-    node = add_two_numbers_from_lists(list1.root, list2.root)
-    assert node.val == '0'
+    node = add_two_numbers_from_lists(list1, list2)
+    assert node.val == 0
 
 
 def test_third_case():
@@ -87,12 +96,47 @@ def test_third_case():
 
     list2 = build_list('9999')
 
-    node = add_two_numbers_from_lists(list1.root, list2.root)
-    assert node.val == '8'
-    assert node.next.val == '9'
-    assert node.next.next.val == '9' 
-    assert node.next.next.next.val == '9' 
-    assert node.next.next.next.next.val == '0' 
-    assert node.next.next.next.next.next.val == '0' 
-    assert node.next.next.next.next.next.next.val == '0' 
-    assert node.next.next.next.next.next.next.next.val == '1' 
+    head = add_two_numbers_from_lists(list1, list2)
+    assert head.val == 8
+    assert head.next.val == 9
+    assert head.next.next.val == 9 
+    assert head.next.next.next.val == 9
+    assert head.next.next.next.next.val == 0
+    assert head.next.next.next.next.next.val == 0 
+    assert head.next.next.next.next.next.next.val == 0
+    assert head.next.next.next.next.next.next.next.val == 1
+
+def test_super_uneven():
+    list1 = build_list('1')
+    list2 = build_list('9999999999')
+
+    head = add_two_numbers_from_lists(list1, list2)
+    assert head.val == 0
+    assert head.next.val == 0
+    assert head.next.next.val == 0
+    assert head.next.next.next.val == 0
+    assert head.next.next.next.next.val == 0
+    assert head.next.next.next.next.next.val == 0 
+    assert head.next.next.next.next.next.next.val == 0
+    assert head.next.next.next.next.next.next.next.val == 0
+    assert head.next.next.next.next.next.next.next.next.val == 0 
+    assert head.next.next.next.next.next.next.next.next.next.val == 0
+    assert head.next.next.next.next.next.next.next.next.next.next.val == 1
+
+
+def test_flopped_super_uneven():
+    list1 = build_list('1')
+    list2 = build_list('9999999999')
+
+    head = add_two_numbers_from_lists(list2, list1)
+    assert head.val == 0
+    assert head.next.val == 0
+    assert head.next.next.val == 0
+    assert head.next.next.next.val == 0
+    assert head.next.next.next.next.val == 0
+    assert head.next.next.next.next.next.val == 0 
+    assert head.next.next.next.next.next.next.val == 0
+    assert head.next.next.next.next.next.next.next.val == 0
+    assert head.next.next.next.next.next.next.next.next.val == 0 
+    assert head.next.next.next.next.next.next.next.next.next.val == 0
+    assert head.next.next.next.next.next.next.next.next.next.next.val == 1
