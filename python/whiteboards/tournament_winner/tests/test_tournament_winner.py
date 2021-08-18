231cac9b89a05eef17499e5f1ce574bc6061d9b1
Beasley <beasley.com@gmail.com>
2021-08-18 12:06:18 -0700

    wrote and passed basic tests

--- python/whiteboards/tournament_winner/tests/test_tournament_winner.py
+++ python/whiteboards/tournament_winner/tests/test_tournament_winner.py
@@ -1,6 +1,26 @@
 from . tournament_winner import tournament_winner
-def test_import_of_name():
-    expected = 'True'
-    actual = tournament_winner(True)
+
+def test_first_case():
+
+    competitions = [
+    ["HTML", "C#"],
+    ["C#", "Python"],
+    ["Python", "HTML"],
+    ]
+
+    results = [0, 0, 1]
+    actual = tournament_winner(competitions, results)
+    expected = 'Python'
+    assert actual == expected
+    
+def test_second_case():
+
+    competitions = [
+    ["HTML", "C#"],
+    ]
+
+    results = [0]
+    actual = tournament_winner(competitions, results)
+    expected = 'C#'
     assert actual == expected
     
