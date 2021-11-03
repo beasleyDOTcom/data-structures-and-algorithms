6863f1ba21e6326e408546c27aa423f7fa7b5c78
Beasley <beasley.com@gmail.com>
2021-11-02 19:40:46 -0700

    cleaned up logging from sort.test.js

--- javascript/challenges/401/sort/__tests__/sort.test.js
+++ javascript/challenges/401/sort/__tests__/sort.test.js
@@ -4,18 +4,15 @@ const sort = require('../sort.js');
 
 it('should sort the array', () => {
     let myArr = [8,4,23,42,16,15];
-    console.log(myArr)
     expect(sort(myArr)).toStrictEqual([4,8,15,16,23,42])
 });
 
 it('should sort the reverse sorted array', () => {
     let myArr = [20,18,12,8,5,-2];
-    console.log(myArr)
     expect(sort(myArr)).toStrictEqual([-2,5,8,12,18,20])
 });
 
 it('should sort the few uniques array', () => {
     let myArr = [5,12,7,5,5,7];
-    console.log(myArr)
     expect(sort(myArr)).toStrictEqual([5,5,5,7,7,12])
 });
